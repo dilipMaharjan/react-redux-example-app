bca435fd0470eeae2538db8ed046507dcf9e3f12
dilipMaharjan <dilipmaharjann@gmail.com>
2017-09-06 14:07:26 -0400

    store

---
+++ src/js/store.js
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+import rootReducer from "./reducers/index";
+
+const middleware = applyMiddleware(promise, thunk, logger);
+export default createStore(rootReducer, middleware);
